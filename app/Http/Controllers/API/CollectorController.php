6b282f4f1176e59721f001712243e924cc22eab0
NathalieAmmoun <nathalie.ammoun98@gmail.com>
2021-11-05 21:15:15 +0200

    fixed an api

--- app/Http/Controllers/API/CollectorController.php
+++ app/Http/Controllers/API/CollectorController.php
@@ -125,7 +125,8 @@ public function getUnapprovedCollectors(){
     $results = array();
     for($i = 0; $i < count($collectors); $i++){
         $results[$i]["collectors"] = $collectors[$i];
-        $results[$i]["recyclables"] = $recyclables[$i];}                             
+        if(count($recyclables)>i){
+        $results[$i]["recyclables"] = $recyclables[$i];}}                             
         return json_encode($results,JSON_PRETTY_PRINT);  
 } 
 
